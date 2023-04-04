b1937441636b389a9a48287b3dc380969ab8f937
vinit <vinitkkulkarni@gmail.com>
2023-04-04 21:32:35 +0530

    Added:ifelse digit to word

---
+++ IfExample1.java
@@ -0,0 +1,41 @@
+package com.bridgelabz.BasicProblems;
+
+public class IfExample1 {
+    public static void main(String[] args) {
+        //1. Read a single Digit Number and write in word
+        int number = 1;
+        if(number == 1){
+            System.out.println("One");
+        }
+        else if (number == 2){
+            System.out.println("Two");
+        }
+        else if (number == 3){
+            System.out.println("Three");
+        }
+        else if (number == 4){
+            System.out.println("Four");
+        }
+        else if (number == 5){
+            System.out.println("Five");
+        }
+        else if (number == 6){
+            System.out.println("Six");
+        }
+        else if (number == 7){
+            System.out.println("Seven");
+        }
+        else if (number == 8){
+            System.out.println("Eight");
+        }
+        else if (number == 9){
+            System.out.println("Nine");
+        }
+        else if (number == 0){
+            System.out.println("Zero");
+        }
+        else {
+            System.out.println("The entered number is not single digit");
+        }
+    }
+}
