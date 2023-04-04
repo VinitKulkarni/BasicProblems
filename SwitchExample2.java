690e44e7791b2238d74c7750158b137a4a5573c3
vinit <vinitkkulkarni@gmail.com>
2023-04-04 21:37:43 +0530

    Added:month name for the given number using switchcase

---
+++ SwitchExample2.java
@@ -0,0 +1,50 @@
+package com.bridgelabz.BasicProblems;
+
+public class SwitchExample2 {
+    public static void main(String[] args) {
+        //2. Write a Program to print the month name for the given number
+
+        int monthNumber = 2;
+        switch(monthNumber){
+            case 1:
+                System.out.println("Jan");
+                break;
+            case 2:
+                System.out.println("Feb");
+                break;
+            case 3:
+                System.out.println("Mar");
+                break;
+            case 4:
+                System.out.println("Apr");
+                break;
+            case 5:
+                System.out.println("May");
+                break;
+            case 6:
+                System.out.println("Jun");
+                break;
+            case 7:
+                System.out.println("Jul");
+                break;
+            case 8:
+                System.out.println("Aug");
+                break;
+            case 9:
+                System.out.println("Sep");
+                break;
+            case 10:
+                System.out.println("Oct");
+                break;
+            case 11:
+                System.out.println("Nov");
+                break;
+            case 12:
+                System.out.println("Dec");
+                break;
+            default:
+                System.out.println("Invalid month number");
+                break;
+        }
+    }
+}
